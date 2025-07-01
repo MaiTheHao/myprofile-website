0ad54c662de70cd9be22c27f116b4073c21b3a6f
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-07-02 06:30:24 +0700

    chore(add prj)

--- src/const/pages/portfolio.ts
+++ src/const/pages/portfolio.ts
@@ -7,18 +7,26 @@ export const PORTFOLIO_ITEMS: ProjectCardInterface[] = [
 		title: 'Neo-Profile Website',
 		category: 'Web Development',
 		imageUrl: '/imgs/Portfolio/My_Profile.jpg',
-		description:
-			'A personal website designed to showcase my skills, projects, and career journey while connecting with professionals in the tech industry.',
+		description: 'A personal website designed to showcase my skills, projects, and career journey while connecting with professionals in the tech industry.',
 		technologies: ['Next.js', 'Figma', 'Vercel'],
 		link: 'https://neo-profile.vercel.app/home',
+	},
+	{
+		id: 4,
+		title: 'Huni E-Commerce Website',
+		category: 'Web Development',
+		imageUrl: '/imgs/Portfolio/Huni_E_Commerce.png',
+		description:
+			'An e-commerce website for Huni Keyboards, featuring product listings, shopping cart, and responsive design. Built to enhance the online shopping experience for mechanical keyboard enthusiasts.',
+		technologies: ['Next.js', 'TypeScript', 'Vercel'],
+		link: 'https://huni-e-commerce.vercel.app',
 	},
 	{
 		id: 2,
 		title: 'Van Nang Mechanical Ltd. Landing Page',
 		category: 'Web Development',
 		imageUrl: '/imgs/Portfolio/Van_Nang.jpg',
-		description:
-			'A marketing landing page designed to attract investors and enhance brand awareness for Van Nang Mechanical Ltd. I was responsible for both design and development.',
+		description: 'A marketing landing page designed to attract investors and enhance brand awareness for Van Nang Mechanical Ltd. I was responsible for both design and development.',
 		technologies: ['Next.js', 'Figma', 'MongoDB', 'Vercel'],
 		link: 'https://vannang.vercel.app',
 	},
